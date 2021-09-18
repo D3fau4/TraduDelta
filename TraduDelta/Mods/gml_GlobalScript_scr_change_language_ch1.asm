7761466b4cf210120c115a64ee324fa72bf0cc5b
D3fau4 <19638408+D3fau4@users.noreply.github.com>
2021-09-18 15:43:35 +0200

    Add project files.

---
+++ TraduDelta/Mods/gml_GlobalScript_scr_change_language_ch1.asm
@@ -0,0 +1,64 @@
+.localvar 2 arguments
+
+:[0]
+b [7]
+
+> gml_Script_scr_change_language_ch1 (locals=0, argc=0)
+:[1]
+pushglb.v global.lang
+push.s "en"@9324
+cmp.s.v EQ
+bf [3]
+
+:[2]
+push.s "es"@58265
+pop.v.s global.lang
+b [6]
+
+:[3]
+pushglb.v global.lang
+push.s "es"@58265
+cmp.s.v EQ
+bf [5]
+
+:[4]
+push.s "ja"@6044
+pop.v.s global.lang
+b [6]
+
+:[5]
+push.s "en"@9324
+pop.v.s global.lang
+
+:[6]
+push.s "true_config.ini"@11939
+conv.s.v
+call.i gml_Script_ossafe_ini_open_ch1(argc=1)
+popz.v
+pushglb.v global.lang
+push.s "LANG"@11940
+conv.s.v
+push.s "LANG"@11940
+conv.s.v
+call.i ini_write_string(argc=3)
+popz.v
+call.i gml_Script_ossafe_ini_close_ch1(argc=0)
+popz.v
+call.i gml_Script_ossafe_savedata_save_ch1(argc=0)
+popz.v
+call.i gml_Script_scr_84_init_localization_ch1(argc=0)
+popz.v
+exit.i
+
+:[7]
+push.i gml_Script_scr_change_language_ch1
+conv.i.v
+pushi.e -1
+conv.i.v
+call.i method(argc=2)
+dup.v 0
+pushi.e -1
+pop.v.v [stacktop]self.scr_change_language_ch1
+popz.v
+
+:[end]
---
+++ TraduDelta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TraduDelta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

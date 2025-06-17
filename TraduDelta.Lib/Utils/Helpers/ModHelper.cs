06de8d90f3c3c640dca54b867af06fb6ac90fce8
Raúl Álvarez <19638408+D3fau4@users.noreply.github.com>
2025-06-17 17:11:26 +0200

    Eliminado marca de agua.

--- TraduDelta.Lib/Utils/Helpers/ModHelper.cs
+++ TraduDelta.Lib/Utils/Helpers/ModHelper.cs
@@ -31,11 +31,7 @@ public class ModHelper : IHelper
                     mod.ApplyMods();
                     AnsiConsole.MarkupLine("[Green]Done![/]");
                     AnsiConsole.MarkupLine("Saving changes...");
-                    if (true)
-                        Data.Strings.MakeString(
-                            "4275696c6420636f6d70696c61646f206578706cc3ad636974616d656e74652070617261206c612074726164756" +
-                            "36369c3b36e2064656c2065717569706f204454532070617261207375732070726f796563746f73207075626c6963616" +
-                            "46f7320656e205472616475537175617265");
+
                     var build = new GameWin(Data);
                     build.save(path);
                     AnsiConsole.MarkupLine("[Green]Done! " + path.Replace(".win", ".mod.win") + " was saved.[/]");
